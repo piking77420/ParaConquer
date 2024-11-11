833bd7c10e416fe65d4078b882f2f975cafe49cc
g.bon <g.bon@student.isartdigital.com>
2024-11-11 11:53:53 +0100

    issue for glslang release link

--- VulkanAPI/src/include/front_end/vulkan_shader_compiler.hpp
+++ VulkanAPI/src/include/front_end/vulkan_shader_compiler.hpp
@@ -10,18 +10,18 @@ namespace Vulkan
     class VulkanShaderCompiler
     {
     public:
-        VulkanShaderCompiler();
+        VULKAN_API VulkanShaderCompiler();
 
-        ~VulkanShaderCompiler();
+        VULKAN_API ~VulkanShaderCompiler();
 
-        void CreateModuleFromSource(vk::Device _device, const char* _source, const char* _path,
+        VULKAN_API void CreateModuleFromSource(vk::Device _device, const char* _source, const char* _path,
             PC_CORE::ShaderStageType _lowLevelShaderStage, SpvReflectShaderModule* _ReflectedModule,
             vk::ShaderModule* _shaderModule);
 
     private:
 
-        std::string IncludePath(const std::string& source, const std::filesystem::path& path);
+        VULKAN_API std::string IncludePath(const std::string& source, const std::filesystem::path& path);
 
-        std::string GetFileAsString(const std::filesystem::path& path);
+        VULKAN_API std::string GetFileAsString(const std::filesystem::path& path);
     };
 }
