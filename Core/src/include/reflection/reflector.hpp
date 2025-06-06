70b6326491342e64a1e2a1a55d04ec98ca32ef73
piking <bon.gabriel77@gmail.com>
2025-06-06 17:14:24 +0200

    Revert "Remove uneccesry check if a member of this name exist"
    This reverts commit c8738518254f4bd7ddf71f991fd888d8d8fcfab3.

--- Core/src/include/reflection/reflector.hpp
+++ Core/src/include/reflection/reflector.hpp
@@ -323,8 +323,14 @@ uint8_t Reflector::ReflectMember(size_t _offset, const char* _memberName)
 	{
 		AddType<MemberType>();
 	}
-
 	const uint32_t holderKey = GetTypeKey<Holder>();
+	for (auto&& member : memberMap.at(holderKey).metaData.members)
+	{
+		// is there aldready a member name as
+		if (member.membersName == _memberName)
+			return 0;
+
+	}
 
 	// Add to sub member
 	const Members members =
