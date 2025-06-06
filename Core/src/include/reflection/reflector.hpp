71199199140fc10bc0e952b39543fe238c911466
piking <bon.gabriel77@gmail.com>
2025-06-06 17:31:15 +0200

    [FIX] iteratorion over member with const &

--- Core/src/include/reflection/reflector.hpp
+++ Core/src/include/reflection/reflector.hpp
@@ -324,7 +324,7 @@ uint8_t Reflector::ReflectMember(size_t _offset, const char* _memberName)
 		AddType<MemberType>();
 	}
 	const uint32_t holderKey = GetTypeKey<Holder>();
-	for (auto&& member : memberMap.at(holderKey).metaData.members)
+	for (const auto& member : memberMap.at(holderKey).metaData.members)
 	{
 		// is there aldready a member name as
 		if (member.membersName == _memberName)
