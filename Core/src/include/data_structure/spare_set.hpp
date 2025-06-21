2e743882cf2e6648047bf6adc2f36d0047726ff9
piking <bon.gabriel77@gmail.com>
2025-06-21 12:59:37 +0200

    [Add] sparset func

--- Core/src/include/data_structure/spare_set.hpp
+++ Core/src/include/data_structure/spare_set.hpp
@@ -3,41 +3,73 @@
 #include <vector>
 
 #include "core_header.hpp"
+#include "reflection/reflector.hpp"
 
 BEGIN_PCCORE
-
-
-    /**
- * 
- * @tparam _Nbr 
- */
-template <uint32_t _Nbr>
+template <class T>
 class SpareSet
 {
 public:
+    void Add(size_t _sparseId, const T& _value);
 
-    static constexpr uint32_t NULL_INDEX = std::numeric_limits<uint32_t>::max();
+    void Remove(size_t _sparseId);
 
-    static_assert(_Nbr < NULL_INDEX, "_Nbr should be lower than uint32_t max");
+    using iterator = typename std::vector<T>::iterator;
+    using const_iterator = typename std::vector<T>::const_iterator;
+    
+    iterator begin() { return m_Dense.begin(); }
+    iterator end() { return m_Dense.end(); }
 
-    uint8_t* Allocate(uint32_t sparseIndex)
-    {
-        
-    }
+    const_iterator begin() const { return m_Dense.begin(); }
+    const_iterator end() const { return m_Dense.end(); }
 
-    SpareSet()
-    {
-        m_Indices.resize(_Nbr);
-    }
+    const_iterator cbegin() const { return m_Dense.cbegin(); }
+    const_iterator cend() const { return m_Dense.cend(); }
 
 private:
-    std::vector<uint8_t> m_Data;
+    std::vector<uint8_t> m_Dense;
     
-    /**
-     * Vector indicies to data
-     * this vector should be preallocated
-     */
-   std::vector<uint32_t> m_Indices;
+    std::vector<size_t> m_Sparse;
+
 };
+
+template <class T>
+void SpareSet<T>::Add(size_t _sparseId, const T& _value)
+{
+    if (_sparseId >= m_Sparse.size())
+        m_Sparse.resize(_sparseId + 1, std::numeric_limits<size_t>::max()); // default to invalid
+
+    // If already present, update the value
+    if (m_Sparse[_sparseId] != std::numeric_limits<size_t>::max())
+    {
+        m_Dense[m_Sparse[_sparseId]] = _value;
+        return;
+    }
+
+    size_t denseIndex = m_Dense.size();
+    m_Sparse[_sparseId] = denseIndex;
+    m_Dense.push_back(_value);
+}
+
+template <class T>
+void SpareSet<T>::Remove(size_t _sparseId)
+{
+    if (_sparseId >= m_Sparse.size() || m_Sparse[_sparseId] == std::numeric_limits<size_t>::max())
+        return; // Not present
+
+    size_t denseIndex = m_Sparse[_sparseId];
+    size_t lastDenseIndex = m_Dense.size() - 1;
+
+    if (denseIndex != lastDenseIndex)
+    {
+        std::swap(m_Dense[denseIndex], m_Dense[lastDenseIndex]);
+        // update current indicies
+        m_Sparse[lastDenseIndex] = denseIndex;
+    }
+
+    m_Dense.pop_back();
+    m_Sparse[_sparseId] = std::numeric_limits<size_t>::max();
+}
+
 
 END_PCCORE
