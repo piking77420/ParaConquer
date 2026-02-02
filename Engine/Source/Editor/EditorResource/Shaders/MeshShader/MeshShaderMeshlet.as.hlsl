f03f5cf7671a84e97bcbea805c72f9f0a6c080c4
piking77420 <bon.gabriel77@gmail.com>
2026-02-02 22:03:38 +0100

    [Fix]mesh shader

--- Engine/Source/Editor/EditorResource/Shaders/MeshShader/MeshShaderMeshlet.as.hlsl
+++ Engine/Source/Editor/EditorResource/Shaders/MeshShader/MeshShaderMeshlet.as.hlsl
@@ -3,17 +3,22 @@
 
 groupshared Payload sPayload;
 
-
 [numthreads(AS_GROUP_SIZE, 1, 1)]
 void Main(
     uint3 gtid : SV_GroupThreadID,
     uint3 dtid : SV_DispatchThreadID,
     uint3 gid : SV_GroupID
-)   
+)
 {
-    if (dtid.x >= DrawCall.SubMeshMeshletCount)
-        return;
+
+    
     sPayload.MeshletIndices[gtid.x] = dtid.x;
-    // Assumes all meshlets are visible
-    DispatchMesh(AS_GROUP_SIZE, 1, 1, sPayload);
+
+    // Last group may have fewer meshlets
+    uint remaining = DrawCall.SubMeshMeshletCount - gid.x * AS_GROUP_SIZE;
+
+    uint dispatchCount = min(remaining, (uint) AS_GROUP_SIZE);
+
+    DispatchMesh(dispatchCount, 1, 1, sPayload);
+    
 }
